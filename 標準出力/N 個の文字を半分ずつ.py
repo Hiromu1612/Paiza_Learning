10adb3f44ba2dbf5d85e16bf1a6fa7b9eccdc010
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-04-06 01:11:44 +0900

    add

---
+++ 標準出力/N 個の文字を半分ずつ.py
@@ -0,0 +1,16 @@
+N=int(input())
+M=N/2
+for i in range(1,N+1):
+    if i==M:
+        print(i,end="\n")
+    else:
+        print(i,end=" ")
+
+#解答
+N = int(input())
+
+for i in range(1, N + 1):
+    if i % (N // 2) == 0: #2で割った商(整数部分)が出力される
+        print(i)
+    else:
+        print(i, end=" ")
