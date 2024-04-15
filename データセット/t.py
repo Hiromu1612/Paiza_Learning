5623cd8a7773cbfb598b864e584b010d03334a65
Hiromu1612 <150511546+Hiromu1612@users.noreply.github.com>
2024-04-15 15:41:39 +0900

    add

--- データセット/t.py
+++ データセット/t.py
@@ -1,13 +1,5 @@
-N,Q=map(int,input().split())
-A=[int(i) for i in input().split()]
-for _ in range(Q):
-    s=[int(i) for i in input().split()]
-    if s[0]==0:
-        A.append(s[1])
-        print(A)
-    elif s[0]==1:
-        A.pop()
-        print(A)
-    elif s[0]==2:
-        print()
-        
+H,W,r,c=map(int,input().split())
+s=[]
+for i in range(H):
+    s.append(input())
+if s[r-1][c-1]=="#":
